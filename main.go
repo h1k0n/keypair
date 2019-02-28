8374a87431eee7e627860c6baf11bbd8d1e213a6
h1k0n <ybzjyhk@gmail.com>
2019-02-28 07:59:57 -0500

    wireguard key

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"golang.org/x/crypto/curve25519"
-  "flag"
 )
 
 func main() {
-        in:=flag.String("in","","in base64")
-       flag.Parse() 
-	//str := "sCecN+FH+pIPL4j2yheiN9n4IJbXg9gRUofTPGf/EXA="
-        str:=*in
-	data, err := base64.StdEncoding.DecodeString(str)
+
+	var b [32]byte
+	_, err := rand.Read(b[:])
 	if err != nil {
 		fmt.Println("error:", err)
 		return
 	}
-	var src, dst [32]byte
-	copy(src[:], data)
-	curve25519.ScalarBaseMult(&dst, &src)
+	fmt.Println(PrintPriKey(b))
+	var dst [32]byte
+	curve25519.ScalarBaseMult(&dst, &b)
 	str1 := base64.StdEncoding.EncodeToString(dst[:])
 	fmt.Println(str1)
 
+}
+
+func PrintPriKey(e [32]byte) string {
+	e[0] &= 248
+	e[31] &= 127
+	e[31] |= 64
+	str1 := base64.StdEncoding.EncodeToString(e[:])
+	return str1
 }
